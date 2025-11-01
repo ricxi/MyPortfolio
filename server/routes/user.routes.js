513b3bd486880db7e78ec163574cdad675fcabf9
ricxi <irixoc@gmail.com>
2025-10-31 23:33:55 -0400

    cleaned up a few things

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -36,20 +36,4 @@ router.delete(
 
 router.delete('/api/users', authCtrl.requireSignin, userCtrl.removeAll);
 
-// router
-//   .route('/api/users/:userId')
-//   .get(userCtrl.read)
-//   .put(authCtrl.hasAuthorization, userCtrl.update)
-//   .delete(authCtrl.hasAuthorization, userCtrl.remove);
-
-// .get(authCtrl.requireSignin, userCtrl.read)
-// .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
-// .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.remove);
-
-// router.route('/api/users').get(userCtrl.list);
-// router.param('userId', userCtrl.userByID);
-// router.route('/api/users/:userId').get(userCtrl.read);
-// router.route('/api/users/:userId').put(userCtrl.update);
-// router.route('/api/users/:userId').delete(userCtrl.remove);
-
 module.exports = router;
